2786ea743bf0aaaf87fe7417af226f1012b48a52
Julian Bajo <j.bajo@outlook.com>
2022-08-16 19:36:22 +0200

    Diseño responsive para el menu y el contenido de la web

--- assets/ui.js
+++ assets/ui.js
@@ -1,1 +1,10 @@
 import './styles/ui.css'
+
+document.querySelectorAll('.opcion-menu').forEach(el => {
+    el.addEventListener('click', () => {
+        let opcionActiva = document.querySelector('.opcion-menu-activa');
+        if (opcionActiva) opcionActiva.classList.remove('opcion-menu-activa');
+
+        el.classList.add('opcion-menu-activa');
+    })
+})
