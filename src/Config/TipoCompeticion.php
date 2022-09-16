2933fa7f5c845c4e3cc1dca4e86abe49de15dc76
Julian Bajo <j.bajo@outlook.com>
2022-09-16 19:10:54 +0200

    Refactorizando el nombre de los tipos de competicion y agrendo un nuevo tipo de competicion para fases de grupos con un unico enfrentamiento entre los equipos

--- src/Config/TipoCompeticion.php
+++ src/Config/TipoCompeticion.php
@@ -5,15 +5,17 @@ namespace App\Config;
 enum TipoCompeticion: string
 {
     case Liga = 'LIGA';
-    case TorneoConLiguilla = 'TORNEO_CON_LIGA';
+    case TorneoConGruposDobles = 'TORNEO_CON_GRUPOS_DOBLES';
+    case TorneoConGruposSimples = 'TORNEO_CON_GRUPOS_SIMPLES';
     case TorneoSoloConEliminatorias = 'TORNEO_KO';
 
     public function nameForSelect(): string
     {
         return match ($this) {
             self::Liga => 'Liga',
-            self::TorneoConLiguilla => 'Torneo con fase de grupos',
             self::TorneoSoloConEliminatorias => 'Sólo eliminatorias',
+            self::TorneoConGruposDobles => 'Torneo con fase de grupos de doble enfrentamiento',
+            self::TorneoConGruposSimples => 'Torneo con fase de grupos de único enfrentamiento',
         };
     }
 }
