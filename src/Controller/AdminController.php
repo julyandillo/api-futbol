b5beabeb8e38e8838eabf2c873f5b390a47909b9
Julian Bajo <j.bajo@outlook.com>
2022-11-26 14:15:53 +0100

    Vista con todos los usuarios registrados. Solo muestra los usuarios registrado (no los usuarios creados con cada aplicacion)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +22,11 @@ class AdminController extends AbstractController
     #[Route('/admin/usuarios', name: 'app_admin_usuarios')]
     public function usuarios(UserRepository $userRepository): Response
     {
-        return $this->render('usuarios/usuarios.html.twig', ['usuarios' => $userRepository->findAll()]);
+        return $this->render('usuarios/usuarios.html.twig', [
+            'usuarios' => array_filter($userRepository->findAll(), function (User $usuario) {
+                return in_array('ROLE_USER', $usuario->getRoles());
+            }),
+        ]);
     }
 
 }
