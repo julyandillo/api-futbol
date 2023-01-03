0c3b6222c30731817ee85bb642dbde38dba8872b
Julián Bajo <j.bajo@outlook.com>
2023-01-03 20:16:14 +0100

    Trait para comprobar parámetros obligatorios modificado, ahora tiene un método para devolver los parámetros que faltan

--- src/Util/CompruebaParametrosTrait.php
+++ src/Util/CompruebaParametrosTrait.php
@@ -6,8 +6,20 @@ use Symfony\Component\HttpFoundation\Request;
 
 trait CompruebaParametrosTrait
 {
+    private array $parametrosFaltantes = [];
+
     public function peticionConParametrosObligatorios(array $parametrosObligatorios, Request $request): bool
     {
-        return empty(array_diff($parametrosObligatorios, array_keys(json_decode($request->getContent(), true))));
+        $this->parametrosFaltantes = array_diff(
+            $parametrosObligatorios,
+            array_keys(json_decode($request->getContent(), true))
+        );
+
+        return empty($this->parametrosFaltantes);
+    }
+
+    public function getParametrosObligatoriosFaltantes(): array
+    {
+        return $this->parametrosFaltantes;
     }
 }
