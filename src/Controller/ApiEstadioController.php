39f81cce07399cb07310d6868d6d13ec2d00d9aa
Julián Bajo <j.bajo@outlook.com>
2023-01-03 20:40:59 +0100

    Controlador de estadios con métodos para crear, ver, modificar, eliminar y listar todos los estadios.

--- src/Controller/ApiEstadioController.php
+++ src/Controller/ApiEstadioController.php
@@ -2,17 +2,120 @@
 
 namespace App\Controller;
 
+use App\Entity\Estadio;
+use App\Repository\EstadioRepository;
+use App\Util\CompruebaParametrosTrait;
+use App\Util\ParseaPeticionJsonTrait;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/api/estadio', name: 'api_estadio_')]
 class ApiEstadioController extends AbstractController
 {
-    #[Route('/api/estadio', name: 'app_api_estadio')]
+    use CompruebaParametrosTrait;
+    use ParseaPeticionJsonTrait;
+
+    public function __construct(private readonly EstadioRepository $estadioRepository)
+    {
+    }
+
+    #[Route('/todos', name: 'listar', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('api_estadio/index.html.twig', [
-            'controller_name' => 'ApiEstadioController',
+        $normalizer = new ObjectNormalizer(new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader())));
+
+        return $this->json(array_map(function (Estadio $estadio) use ($normalizer) {
+                return $normalizer->normalize($estadio, null, ['groups' => 'lista']);
+            }, $this->estadioRepository->findAll())
+        );
+    }
+
+    #[Route('/{idEstadio}', methods: ['GET'])]
+    public function detalles(int $idEstadio, NormalizerInterface $normalizer): Response
+    {
+        $estadio = $this->estadioRepository->find($idEstadio);
+
+        if (!$estadio) {
+            return $this->json([
+                'msg' => 'No existe ningún estadio con el id ' . $idEstadio,
+            ], 204);
+        }
+
+        return $this->json($normalizer->normalize($estadio));
+    }
+
+    #[Route(name: 'nuevo', methods: ['POST'])]
+    public function nuevo(Request $request, SerializerInterface $serializer): Response
+    {
+        $camposObligatorios = ['nombre', 'ciudad', 'capacidad'];
+        if (!$this->peticionConParametrosObligatorios($camposObligatorios, $request)) {
+            return $this->json([
+                'msg' => sprintf('Faltan parámetros obligatorios para crear el estadio: [%s]',
+                    implode(', ', $this->getParametrosObligatoriosFaltantes()))
+            ]);
+        }
+
+        $this->parseaContenidoPeticionJson($request);
+
+        $estadio = $this->estadioRepository->findOneBy([
+            'nombre' => $this->contenidoPeticion['nombre'],
+        ]);
+
+        if ($estadio) {
+            return $this->json([
+                'msg' => sprintf('Ya existe un estadio con el nombre \'%s\'', $this->contenidoPeticion['nombre']),
+            ], 502);
+        }
+
+        $estadio = $serializer->deserialize($request->getContent(), Estadio::class, 'json');
+        $this->estadioRepository->save($estadio, true);
+
+        return $this->json(['msg' => 'Estadio creado correctamente']);
+    }
+
+    #[Route('/{idEstadio}', methods: ['PATCH'])]
+    public function edita(int $idEstadio, Request $request, SerializerInterface $serializer): Response
+    {
+        $estadio = $this->estadioRepository->find($idEstadio);
+
+        if (!$estadio) {
+            return $this->json([
+                'msg' => 'No existe ningún estadio con el id ' . $idEstadio,
+            ], 204);
+        }
+
+        $serializer->deserialize($request->getContent(), Estadio::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $estadio]);
+        $this->estadioRepository->save($estadio, true);
+
+        return $this->json([
+            'msg' => 'Estadio actualizado correctamente',
+        ]);
+    }
+
+    #[Route('/{idEstadio}', methods: ['DELETE'])]
+    public function elimina(int $idEstadio): Response
+    {
+        $estadio = $this->estadioRepository->find($idEstadio);
+
+        if (!$estadio) {
+            return $this->json([
+                'msg' => 'No existe ningún estadio con el id ' . $idEstadio,
+            ], 204);
+        }
+
+        $this->estadioRepository->remove($estadio, true);
+
+        return $this->json([
+            'msg' => 'Estadio eliminado correctamente',
         ]);
     }
 }
--- src/Entity/Estadio.php
+++ src/Entity/Estadio.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\EstadioRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: EstadioRepository::class)]
 class Estadio
@@ -11,9 +12,11 @@ class Estadio
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups('lista')]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups('lista')]
     private ?string $nombre = null;
 
     #[ORM\Column(length: 255)]
